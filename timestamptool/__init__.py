62c6b345467cbce5f7241a2709ef84536f01f8c0
jakeogh <github.com@v6y.net>
2024-10-20 15:26:41 -0700

    auto-commit

--- timestamptool/__init__.py
+++ timestamptool/__init__.py
@@ -1,2 +1,6 @@
-from .timestamptool import get_int_timestamp
-from .timestamptool import get_timestamp
+"""
+isort:skip_file
+"""
+
+from .timestamptool import get_int_timestamp as get_int_timestamp
+from .timestamptool import get_timestamp as get_timestamp
